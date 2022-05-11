9aa83e210f83af09f521c1f5166eb90825d333de
menna <mennawael867@gmail.com>
2022-05-11 21:46:45 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace SmallWorldPhenomenon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Small world phenomenon:\n" +
+                              "[1] Sample test cases\n" +
+                              "[2] Complete testing");
+            Console.Write("\nEnter your choice [1-2]: ");
+            char choice = (char)Console.ReadLine()[0];
+            if (choice == '1')
+            {
+                string queries = "Testcases\\Sample\\queries1.txt";
+                Smallworld.read_quiries(queries);
+
+                string movie = "Testcases\\Sample\\movies1.txt";
+                Smallworld.read_movies(movie);
+
+            }
+            else if (choice == '2')
+            {
+                string queries1;
+                Console.WriteLine("[1] Small\n" +
+                                  "[2] Medium\n" +
+                                  "[3] Large\n" +
+                                  "[4] Extreme\n" +
+                                  "Enter your choice [1-4]: ");
+                char choice2 = (char)Console.ReadLine()[0];
+                if (choice2 == '1')
+                { 
+                    //Case 1
+                    queries1 = "Testcases\\Complete\\small\\Case1\\queries110.txt";
+                    Smallworld.read_quiries(queries1);
+                    //Case 2
+                    queries1 = "Testcases\\Complete\\small\\Case2\\queries50.txt";
+                    Smallworld.read_quiries(queries1);
+
+                }
+                else if (choice2 == '2')
+                {
+                    //Case 1
+                    queries1 = "Testcases\\Complete\\medium\\Case1\\queries85.txt";
+                    Smallworld.read_quiries(queries1);
+                    queries1 = "Testcases\\Complete\\medium\\Case1\\queries4000.txt";
+                    Smallworld.read_quiries(queries1);
+                    //Case 2
+                    queries1 = "Testcases\\Complete\\medium\\Case2\\queries110.txt";
+                    Smallworld.read_quiries(queries1); 
+                    queries1 = "Testcases\\Complete\\medium\\Case2\\queries2000.txt";
+                    Smallworld.read_quiries(queries1);
+
+                }
+                else if (choice2 == '3')
+                {
+                    //large
+                    queries1 = "Testcases\\Complete\\large\\queries26.txt";
+                    Smallworld.read_quiries(queries1); 
+                    queries1 = "Testcases\\Complete\\large\\queries600.txt";
+                    Smallworld.read_quiries(queries1);
+
+                }
+                else if (choice2 == '4')
+                {
+                    //extreme
+                    queries1 = "Testcases\\Complete\\extreme\\queries22.txt";
+                    Smallworld.read_quiries(queries1);
+                    queries1 = "Testcases\\Complete\\extreme\\queries200.txt";
+                    Smallworld.read_quiries(queries1);
+                }
+                else
+                    Console.WriteLine("Invalide input!");
+            }
+            else
+                Console.WriteLine("Invalide input!");
+        }
+    }
+}
---
+++ Smallworld.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace SmallWorldPhenomenon
+{
+    class Smallworld
+    {
+        public static void read_movies(string path)
+        {
+            //split on /  [0] movie name    [:] actors
+            Dictionary<string, List<string>> movie = new Dictionary<string, List<string>>();
+            List<string> movie_name = new List<string>();
+            string text = File.ReadAllText(path);
+            string[] lineItems = text.Split('\n');
+            for(int i = 0; i < lineItems.Length; i ++)
+            {
+                string[] s = lineItems[i].Split('/');
+                List<string> actors = new List<string>();
+                for (int j = 1; j < s.Length; j++)
+                    actors.Add(s[j]);
+                movie_name.Add(s[0]);
+                movie[s[0]] = actors;
+                Console.WriteLine(movie_name[movie_name.Count - 1]);
+                for(int j = 0; j < movie_name.Count - 1; j ++)
+                     Console.WriteLine(movie[movie_name[i]].);
+            }
+
+        }
+        public static void read_quiries(string path)
+        {
+            string text = File.ReadAllText(path);
+            string[] lineItems = text.Split('\n');
+            List<KeyValuePair<string, string>> list = new List<KeyValuePair<string, string>>();
+            for (int i = 0; i < lineItems.Length - 1; i ++)
+            {
+                string[] s = lineItems[i].Split('/');
+                KeyValuePair<string, string> pair = new KeyValuePair<string, string>(s[0], s[1]);
+                list.Add(pair);
+            }
+        }
+    }
+}
